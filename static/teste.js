2658618af66d52265d2b0ab71005682362e9fc0d
Luiz Fernando <165337963+Luiz28111987@users.noreply.github.com>
2024-08-29 16:58:06 -0300

    sistema web

---
+++ static/teste.js
@@ -0,0 +1,25 @@
+document.getElementById('loginForm').addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const usernameInput = document.getElementById('username');
+  const passwordInput = document.getElementById('password');
+  const errorMessage = document.getElementById('error-message');
+
+  const username = usernameInput.value.trim();
+  const password = passwordInput.value;
+
+  // Validação básica
+  if (!username || !password) {
+    errorMessage.textContent = 'Por favor, preencha todos os campos.';
+    return;
+  }
+
+  // Verificação de credenciais (substitua por sua lógica de autenticação)
+  if (username === 'Admin' && password === 'admin') {
+    // Redirecionamento
+    window.location.href = 'dashboard.html';
+  } else {
+    // Exibe mensagem de erro se as credenciais estiverem incorretas
+    document.getElementById('error-message').textContent = 'Usuário ou senha incorretos!';
+  }
+});
